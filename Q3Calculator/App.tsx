20dfd7d10f744d804bb27c9be08addc0e39f1e40
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2023-10-25 11:19:59 +0700

    Test v1

---
+++ Q3Calculator/App.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import style from "./style";
+export default function App() {
+
+  return (
+    <View style={style.background} >
+      <View style={style.container}>
+
+      </View>
+    </View>
+  )
+}
